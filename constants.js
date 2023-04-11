c9c0292daa2b720d609b8cbec6c1288d44794e2b
SEAF39 <labber@DESKTOP-7J1C764>
2023-04-11 15:06:11 -0400

    reafactoring the code clinet js

---
+++ constants.js
@@ -0,0 +1,8 @@
+// client constants
+const IP = "192.168.100.164";
+const PORT = 50541;
+
+module.exports = {
+  IP,
+  PORT,
+};
---
+++ input.js
@@ -0,0 +1,38 @@
+// Stores the active TCP connection object.
+let connection;
+// setup interface to handle user input from stdin
+const setupInput = function (conn) {
+  connection = conn;
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+  return stdin;
+};
+
+const handleUserInput = function (useInput) {
+  console.log("The snake direction", useInput);  
+  // if statement to determine which direction the snake to move            
+  if (useInput === "w") {
+    connection.write("Move: up");
+  }
+  if (useInput === "a") {
+    connection.write("Move: left");
+  }
+  if (useInput === "x") {
+    connection.write("Move: down");
+  }
+  if (useInput === "d") {
+    connection.write("Move: right");
+  }
+    // if the user presses the p key then will appear on the screen "let us have fun with the snake"
+  if (useInput === "p") {
+    connection.write("Say: let us have fun with the snake");
+  }
+  if (useInput === "\u0003") {
+    process.exit();
+  }
+};
+
+module.exports = { setupInput };
